a0289d6b655e16968b7119e98d33e604403d25e0
ChenSino <chenxk@sonoscape.net>
2022-07-28 18:30:58 +0800

    feat(log): 添加aop

---
+++ common/common-log/src/main/java/com/chensino/common/log/annotation/OperateLog.java
@@ -0,0 +1,24 @@
+package com.chensino.common.log.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 日志注解，启用则记录日志
+ *
+ * @author 204506
+ * @version 1.0
+ * @date 2022-07-28 18:12
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface OperateLog {
+
+    /**
+     * 日志描述
+     * @return {String}
+     */
+    String value();
+}
---
+++ common/common-log/src/main/java/com/chensino/common/log/aspect/OperateLogAspect.java
@@ -0,0 +1,16 @@
+package com.chensino.common.log.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+
+/**
+ * 日志切面
+ *
+ * @author 204506
+ * @version 1.0
+ * @date 2022-07-28 18:25
+ */
+@Slf4j
+@Aspect
+public class OperateLogAspect {
+}
