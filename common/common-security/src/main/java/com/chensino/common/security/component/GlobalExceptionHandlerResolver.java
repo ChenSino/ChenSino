f467c8b443743ccb7cc41bf8a6fb7e435d605121
ChenSino <chenxk@sonoscape.net>
2022-08-04 15:38:09 +0800

    feat(log): 添加404异常捕获
    默认情况下404不会打印异常，需要手动在配置中开启

--- common/common-security/src/main/java/com/chensino/common/security/component/GlobalExceptionHandlerResolver.java
+++ common/common-security/src/main/java/com/chensino/common/security/component/GlobalExceptionHandlerResolver.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -33,16 +34,28 @@ public class GlobalExceptionHandlerResolver {
     }
 
     /**
-     * 全局异常
+     * 参数不匹配异常
      *
      * @param e 异常
      * @return 返回统一实体
      */
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<String> handlerMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e, HttpServletRequest request) {
-        String requestURI = request.getRequestURI();
-        log.error("参数不匹配，请求路径：{} ，exception={}", requestURI, e.getMessage(), e);
+        log.error("参数不匹配，请求路径：{} ，exception={}", request.getRequestURI(), e.getMessage(), e);
+        return ResponseEntity.fail(e.getLocalizedMessage());
+    }
+
+    /**
+     * 404
+     *
+     * @param e 异常
+     * @return 返回统一实体
+     */
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<String> handlerNoHandlerFoundException(NoHandlerFoundException e, HttpServletRequest request) {
+        log.error("请求路径{}不存在 ，exception={}", request.getRequestURI(), e.getMessage(), e);
         return ResponseEntity.fail(e.getLocalizedMessage());
     }
 }
