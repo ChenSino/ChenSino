8b9ba7b0e42cdd7abb61309116e19ffb20d4de18
chenxk <chenxk@sonoscape.net>
2025-01-21 17:37:36 +0800

    feat:更新swagger网页配置，添加请求头鉴权

--- common/common-swagger/src/main/java/com/chensino/common/swagger/configuration/OpenApiConfiguration.java
+++ common/common-swagger/src/main/java/com/chensino/common/swagger/configuration/OpenApiConfiguration.java
@@ -1,11 +1,12 @@
 package com.chensino.common.swagger.configuration;
 
 import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,44 +22,58 @@ public class OpenApiConfiguration {
      * @return openApi 配置信息
      */
     @Bean
-    public OpenAPI springDocOpenAPI() {
-        return new OpenAPI().info(new Info()
-                        .title("ChenSino API")
-                        .description("ChenSino接口文档说明")
-                        .version("v0.0.1-SNAPSHOT")
-                        .license(new License().name("Chensino博客")
-                                .url("https://chensino.github.io")))
-                .externalDocs(new ExternalDocumentation()
-                        .description("GitHub项目地址")
-                        .url("https://github.com/ChenSino/ChenSino"))
-                // 配置Authorizations
-                .components(new Components().addSecuritySchemes("bearer-key",
-                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer")));
+    public OpenAPI customOpenAPI() {
+        Info info = new Info()
+                .title("ChenSino API")
+                .description("ChenSino接口文档说明")
+                .version("v0.0.1-SNAPSHOT")
+                .license(new License().name("Chensino博客")
+                        .url("https://chensino.github.io"));
+        final String securitySchemeName = "xTokenAuth";
+        Components components = new Components()
+                .addSecuritySchemes(securitySchemeName,
+                        new SecurityScheme()
+                                .name("X-token") // 请求头名称
+                                .type(SecurityScheme.Type.APIKEY)
+                                .in(SecurityScheme.In.HEADER) // 请求头位置
+                );
+        return new OpenAPI()
+                .info(info)
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(components);
     }
 
-  /*  *//**
+    /**
      * demo 分组
      *
      * @return demo分组接口
-     *//*
+     */
     @Bean
-    public GroupedOpenApi siteApi() {
+    public GroupedOpenApi loginApi() {
         return GroupedOpenApi.builder()
                 .group("登录接口")
                 .pathsToMatch("/login/**")
                 .build();
     }
 
-    *//**
+    @Bean
+    public GroupedOpenApi userApi() {
+        return GroupedOpenApi.builder()
+                .group("用户管理接口")
+                .pathsToMatch("/user/**")
+                .build();
+    }
+
+    /**
      * minio接口 分组
      *
      * @return minio接口
-     *//*
+     */
     @Bean
     public GroupedOpenApi adminApi() {
         return GroupedOpenApi.builder()
                 .group("minio接口")
                 .pathsToMatch("/minio/**")
                 .build();
-    }*/
+    }
 }
